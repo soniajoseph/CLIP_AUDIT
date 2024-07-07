3a5a7d13dc2e78be71f290e1576f2630b1ccaabd
soniajoseph <soniamollyjoseph@gmail.com>
2024-07-06 21:14:48 -0400

    add random tinyclip indices

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The purpose of this repo is to collect activations when auditing CLIP models.
 We focus on TinyCLIP although the code should be model agnostic.
 
 **Order of Operations** 
-1. `get_neuron_indices.py`: Get random neuron indices from that model
+1. `get_neuron_indices.py`: Get random neuron indices from that model. DONE.
 2. `test_model_accuracy.py`: Test the accuracy of the model on your dataset
 3. `cache_activations.py`: Cache activations and image indices for those neurons
 4. `get_activation_intervals.py`: Define activation intervals and randomly sample n image indices from those intervals
--- get_neuron_indices.py
+++ get_neuron_indices.py
@@ -2,35 +2,41 @@
 from vit_prisma.models.base_vit import HookedViT
 import argparse
 
+from collections import OrderedDict
+
+import numpy as np
 
 
-
-def load_model(model_name):
-    """
-    Load model
-    """
-    model = HookedViT.from_pretrained(model_name, is_timm=False, is_clip=True)
-    return model
-
-
-def main(model_name):
+def main(save_file_name, total_neurons, n, layers):
     """
     Main function
     """
-    model = load_model(model_name)
 
-    print(model)
+    # Dictionary of indices per layer
+    indices_dict = OrderedDict()
 
+    for l in range(layers):
+        # Generate n random numbers out of total number of neurons, no replacement
+        indices_dict[l] = np.random.choice(total_neurons, n, replace=False)
+
+    print(indices_dict)
+
+    # Save dictionary to folder
+    np.save(save_file_name, indices_dict)
+
+    print(f"Saved neuron indices to {save_file_name}")
+
+    return indices_dict
 
 
 # main function
 if __name__ == "__main__":
 
-    # activation parser, model name
-    parser = argparse.ArgumentParser(description='Get neuron indices')
-    parser.add_argument('--model_name', type=str, default="wkcn/TinyCLIP-ViT-40M-32-Text-19M-LAION400M", help='Model name')
+    parser = argparse.ArgumentParser(description="Get neuron indices")
+    parser.add_argument("--save_file_name", default="./saved_data/tinyclip_neuron_indices_mlp_out.npy", type=str, help="Model name")
+    parser.add_argument("--total_neurons", default=512, type=int, help="Total number of neurons")
+    parser.add_argument("--n", default=100, type=int, help="Number of neurons to sample")
+    parser.add_argument("--layers", default=12, type=int, help="Number of layers")
     args = parser.parse_args()
 
-    # main function
-    main(args.model_name)
-
+    main(args.save_file_name, args.total_neurons, args.n, args.layers)
