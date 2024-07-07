15c0655fe2aa713c903b710856ccb5828fa6e5ad
soniajoseph <soniamollyjoseph@gmail.com>
2024-07-06 23:23:04 -0400

    continue working on accuracy sanity check

--- clip_audit/test_model_accuracy.py
+++ clip_audit/test_model_accuracy.py
@@ -8,20 +8,49 @@ Mainly as sanity check and also making sure we're doing our image normalization 
 import os
 from clip_audit.utils.load_imagenet import load_imagenet
 
-
-path = '/network/scratch/s/sonia.joseph/datasets/kaggle_datasets'
-dataloader = load_imagenet(path, 'train')
+import argparse
 
 
+def main(imagenet_path, model_type, model_name):
 
-# iterate through
-MAX = 5
-count = 0
+    path = '/network/scratch/s/sonia.joseph/datasets/kaggle_datasets'
+    dataloader = load_imagenet(path, 'train')
 
-for i, (img, label, idx) in enumerate(dataloader):
-    print(img.shape)
-    print(label)
-    # print(idx)
-    count += 1
-    if count >= MAX:
-        break
+
+
+    # Check original model
+    if model_type == 'original':
+        model = torch.hub.load(model_name, 'ViT-B/32', jit=False)
+    elif model_type == 'hooked':
+        model = torch.hub.load('openai/clip', 'ViT-B/32', jit=False)
+        model = hook_model(model)
+
+    # iterate through
+    MAX = 5
+    count = 200
+
+    with torch.no_grad():
+        for i, (img, label, idx) in enumerate(dataloader):
+            print(img.shape)
+            print(label)
+            # print(idx)
+            count += 1
+            if count >= MAX:
+                break
+
+    
+
+
+# Results are
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser(description='Test accuracy of TinyCLIP on dataset')
+    parser.add_argument('--imagenet_path', type=str, default='/network/scratch/s/sonia.joseph/datasets/kaggle_datasets', help='Path to dataset')
+    parser.add_argument('--model_type', type=str, default='original', help='Model to test (original/hooked)")
+    parser.add_argument('--model_name', type=str, default='openai/clip', help='Model name to test')
+
+    args = parser.parse_args()
+    
+    main(args.imagenet_path, args.model_type, args.model_name)
